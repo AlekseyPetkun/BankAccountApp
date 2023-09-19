4bdc6cea28dece270070004c561bd61f17d5a9ad
Aleksey Petkun <aleksey_2710@mail.ru>
2023-09-19 19:51:02 +0300

    creating BankAccountAppApplication

--- src/main/java/com/github/alekseypetkun/bankaccountapp/BankAccountAppApplication.java
+++ src/main/java/com/github/alekseypetkun/bankaccountapp/BankAccountAppApplication.java
@@ -1,9 +1,15 @@
 package com.github.alekseypetkun.bankaccountapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "BankAccountApplication",
+        description = "Веб-приложение по типу банковского приложения для создания счетов"
+))
 public class BankAccountAppApplication {
 
     public static void main(String[] args) {
