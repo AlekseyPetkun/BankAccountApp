2c3498ee6a16ba8bed335641136dfd93f1800f1f
Aleksey Petkun <aleksey_2710@mail.ru>
2023-09-19 16:20:32 +0300

    initial commit

---
+++ src/main/java/com/github/alekseypetkun/bankaccountapp/BankAccountAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.alekseypetkun.bankaccountapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountAppApplication.class, args);
+    }
+
+}
