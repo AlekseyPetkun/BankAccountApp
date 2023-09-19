0ec0d738dac74dce43e24da243b9c4ac30750c12
Aleksey Petkun <aleksey_2710@mail.ru>
2023-09-19 19:50:04 +0300

    creating sql script

---
+++ src/main/resources/db/migration/V1__19092023.sql
@@ -0,0 +1,7 @@
+CREATE TABLE accounts
+(
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(64) NOT NULL UNIQUE,
+    password VARCHAR(64) NOT NULL,
+    balance  INTEGER     NOT NULL DEFAULT (0)
+);
