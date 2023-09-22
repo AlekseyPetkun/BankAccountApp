0db8253389c5437f1b26fe33bdde2d7b63e6f718
Aleksey Petkun <aleksey_2710@mail.ru>
2023-09-22 20:29:47 +0300

    creating exceptions

--- src/main/java/com/github/alekseypetkun/bankaccountapp/exception/NotFoundException.java
+++ src/main/java/com/github/alekseypetkun/bankaccountapp/exception/NotFoundException.java
@@ -1,4 +1,8 @@
 package com.github.alekseypetkun.bankaccountapp.exception;
 
 public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/github/alekseypetkun/bankaccountapp/exception/RemoveFromBalanceException.java
+++ src/main/java/com/github/alekseypetkun/bankaccountapp/exception/RemoveFromBalanceException.java
@@ -2,14 +2,7 @@ package com.github.alekseypetkun.bankaccountapp.exception;
 
 public class RemoveFromBalanceException extends NotFoundException{
 
-    private final Integer balance;
-
     public RemoveFromBalanceException(Integer balance) {
-        this.balance = balance;
-    }
-
-    @Override
-    public String getMessage() {
-        return String.format("На счете: " + balance + ", что меньше запрашиваемой суммы!");
+        super("На счете: " + balance + ", что меньше запрашиваемой суммы!");
     }
 }
